4d2b7b28af2c5ba5033c1b1e7f6e32ac7fbcfdbc
stan545 <111024073+stan545@users.noreply.github.com>
2023-03-06 09:19:56 +0100

    Organized

--- C files/math.c
+++ C files/math.c
@@ -1,18 +1,23 @@
-#include <math.h>
 #include <stdio.h>
+#include <math.h>
 
-int main(void)
+int main()
 {
+	double a = 5;
+	double b = 25;
+
+	double c = a / b;
+
+	printf("%f\n", c);
+
 	int x;
 	x = 5;
 	
 	int y = x / 2;
 	int remainder = x % 2;
 
-	{
 	printf("%d\n", y);
 	printf("Modulo " "%d\n", remainder);
-	}
 
 	return 0;
 
